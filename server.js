3d9f78d65fe42d4b479bb63895fb757b922c9b5f
swathisk4 <37213686+swathisk4@users.noreply.github.com>
2018-03-09 18:45:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/art', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'art'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
